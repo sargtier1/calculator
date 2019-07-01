579bd4a9b54091f767ce1c2ec3bd10a416839335
JustTams <sarg.tier1@gmail.com>
2019-07-01 04:22:48 -0500

    footer fix

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer>
-      <h5>Made With Love</h5>
       <p>Salvatore Argentieri © </p>
-        {/*  social media wheel  */}
     </footer>
   );
 };
