20742d489b4b0862284c96046216a89c57ed4aa7
JustTams <sarg.tier1@gmail.com>
2019-07-01 00:07:09 -0500

    final styles push

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,21 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Header = () => {
-    return (
-        <header>
-            <StyledTitle>Calculator App</StyledTitle>
-        </header>
-    )
+  return (
+    <header>
+      <StyledTitle>Calculator App</StyledTitle>
+    </header>
+  )
 }
 
 const StyledTitle = styled.h1`
-  border-radius: 30px;
   font-size: 2em;
   text-align: center;
   padding: 3rem;
-  color: azure;
-  background-color: #85c1e9;
+  color: #373737;
+  background: linear-gradient(135deg, #a8edea 0%, #fed6e3 100%);
   box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.2), 0 2px 10px 0 rgba(0, 0, 0, 0.19);
 `
 
-export default Header;
+export default Header
